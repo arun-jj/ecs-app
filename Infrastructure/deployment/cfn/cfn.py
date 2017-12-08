21dcc619252cdf8575a7bb49646ff061ee7bd346
Jude Jesuthasan <jude.jesuthasan@watchguard.com>
2017-12-07 19:02:40 -0500

    initial ecs commit

---
+++ Infrastructure/deployment/cfn/cfn.py
@@ -0,0 +1,84 @@
+import boto3
+import botocore
+import logging
+
+
+class StackException(Exception):
+    pass
+
+
+class CfnStack:
+
+    def __init__(self, profile_name):
+        session = boto3.session.Session(profile_name=profile_name)
+        self._client = session.client('cloudformation')
+
+    def is_stack_exist(self, stackname):
+        try:
+            self._client.describe_stacks(StackName=stackname)
+            return True
+        except botocore.exceptions.ClientError:
+            logging.error('The stack {} doesn not exist'.format(stackname))
+            return False
+
+    def create_stack(self, stackname, template, parameters=None, iam=None):
+        try:
+            params = {
+                'StackName': stackname,
+                'TemplateBody': template,
+                'OnFailure': 'ROLLBACK'
+            }
+            if parameters:
+                params.update({'Parameters': parameters})
+            if iam:
+                if iam not in ['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM']:
+                    msg = 'Invalid value {} for iam'.format(iam)
+                    logging.error(msg)
+                    raise StackException(msg)
+                params.update({'Capabilities': iam})
+
+            self._client.create_stack(**params)
+
+            # create waiter
+            waiter = self._client.get_waiter('stack_create_complete')
+            waiter.wait(StackName=stackname)
+        except botocore.exceptions.ClientError as err:
+            msg = 'The Stack({}) creation failed: {}'.format(stackname, err)
+            logging.error(msg)
+            raise StackException(msg)
+
+    def update_stack(self, stackname, template, parameters=None, iam=None):
+        try:
+            params = {
+                'StackName': stackname,
+                'TemplateBody': template
+            }
+            if parameters:
+                params.update({'Parameters': parameters})
+            if iam:
+                if iam not in ['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM']:
+                    msg = 'Invalid value {} for iam'.format(iam)
+                    logging.error(msg)
+                    raise StackException(msg)
+                params.update({'Capabilities': iam})
+
+            self._client.update_stack(**params)
+
+            # create waiter
+            waiter = self._client.get_waiter('stack_update_complete')
+            waiter.wait(StackName=stackname)
+        except botocore.exceptions.ClientError as err:
+            msg = 'The Stack({}) update failed: {}'.format(stackname, err)
+            logging.error(msg)
+            raise StackException(msg)
+
+    def delete_stack(self, stackname):
+        try:
+            self._client.delete_stack(StackName=stackname)
+
+            waiter = self._client.get_waiter('stack_delete_complete')
+            waiter.wait(StackName=stackname)
+        except botocore.exceptions.ClientError as err:
+            msg = 'The Stack({}) delete failed: {}'.format(stackname, err)
+            logging.error(msg)
+            raise StackException(msg)
--- Infrastructure/deployment/run.py
+++ Infrastructure/deployment/run.py
@@ -1,35 +1,33 @@
-import boto3
-import botocore
+import cfn
 import logging
-
-session = boto3.session.Session(profile_name='local')
-
-client = session.client('cloudformation')
+import os
 
 
-def create_cfn_stack(stack, stackname, template, cfnclient, parameters=None, iam=None):
+def create_or_update_stack(stack: cfn.CfnStack, stackname, template, parameters=None, iam=None):
     """
     create/update cloudformation stack
     """
     logging.info("Received request to create/update stack {!r} with parameters {!r}".format(stackname, str(parameters)))
     try:
-        cfnclient.describe_stacks(StackName=stackname)
-        logging.info("{!r} stack already exists. Checking if update is needed...".format(stackname))
-
-        updateneed = stack.updatestack(stackname=stackname, template=template,
-                                       parameters=parameters, cfnclient=cfnclient, iam=iam)
-        if updateneed:
-            stack.stackstatuswaiter(stackname=stackname,
-                                    status="stack_update_complete",
-                                    cfnclient=cfnclient)
-            logging.info("Stack {!r} update completed.".format(stackname))
+        is_update = stack.is_stack_exist(stackname)
+        if is_update:
+            stack.update_stack(stackname=stackname, template=template,
+                               parameters=parameters, iam=iam)
+            logging.info("Updating stack: {}".format(stackname))
         else:
-            logging.info("Stack {!r} needs no update.".format(stackname))
-    except botocore.exceptions.ClientError as err:
-        logging.info("Creating stack {!r} now...".format(stackname))
-        stack.createstack(stackname=stackname, template=template,
-                          parameters=parameters, cfnclient=cfnclient, iam=iam)
-        stack.stackstatuswaiter(stackname=stackname,
-                                status="stack_create_complete",
-                                cfnclient=cfnclient)
-        logging.info("Stack {!r} creation completed.".format(stackname))
+            logging.info("Creating stack: {}".format(stackname))
+            stack.create_stack(stackname=stackname, template=template,
+                               parameters=parameters, iam=iam)
+    except cfn.StackException as err:
+        logging.error(str(err))
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s -  %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    stack = cfn.CfnStack('local')
+    # vpc template
+    stackname = 'my-network'
+    vpc = open(os.path.join('../CFN', 'vpc.yaml')).read()
+
+    create_or_update_stack(stack, stackname, vpc)
